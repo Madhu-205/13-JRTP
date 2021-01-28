7f6c2a6c6636726ceff696413cf0604a3ec6beea
madhu <madhuediga10@gmail.com>
2021-01-28 10:27:14 +0530

    changes

--- src/main/java/com/madhu/App01Application.java
+++ src/main/java/com/madhu/App01Application.java
@@ -2,12 +2,14 @@ package com.madhu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class App01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(App01Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(App01Application.class, args);
+		System.out.println("madhu");
 	}
 
 }
